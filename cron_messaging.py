4c35ab82d7bc22aaf9fdaaf294ef3e24932c2b9a
Maksym Trineiev <mtrineyev@gmail.com>
2023-04-07 14:38:26 +0300

    Instant message and logging added

--- cron_messaging.py
+++ cron_messaging.py
@@ -1,11 +1,21 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import logging
 import telebot
 import tzlocal
 from settings import BOT_TOKEN, KIDS_GROUP_ID, CONVERSATION_LINK
 
 
+logging.basicConfig(
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    level=logging.INFO
+)
 schedule = BlockingScheduler(timezone=str(tzlocal.get_localzone()))
 bot = telebot.TeleBot(BOT_TOKEN)
+
+
+def instant_message(msg, *args, **kwargs):
+    bot.send_message(KIDS_GROUP_ID, msg, *args, **kwargs)
+    logging.info(f"Sent instant message '{msg}'")
 
 
 def first_reminder():
@@ -15,6 +25,7 @@ def first_reminder():
         "За 15 хв почнеться наше заняття 👩‍💻",
         parse_mode="MarkdownV2"
     )
+    logging.info("Sent first reminder")
 
 
 def second_reminder():
@@ -25,14 +36,16 @@ def second_reminder():
         disable_web_page_preview=True,
         parse_mode="MarkdownV2"
     )
+    logging.info("Sent second reminder")
 
 
 def main():
+    # instant_message("Доброго ранку! Сподіваюся у всіх все чудово "
+    #                 "і побачимося з вами завтра на нашому занятті!")
     schedule.add_job(first_reminder, trigger="cron",
                      day_of_week="Sat", hour=11, minute=15)
     schedule.add_job(second_reminder, trigger="cron",
                      day_of_week="Sat", hour=11, minute=25)
-    print("Have set two cron jobs...")
     schedule.start()
 
 

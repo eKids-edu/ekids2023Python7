609e1587e77bae29b986827a3d5c82f7858aa063
Maksym Trineiev <mtrineyev@gmail.com>
2023-04-20 16:41:27 +0300

    Lesson 6 finished

--- Lesson06/main.py
+++ Lesson06/main.py
@@ -20,10 +20,6 @@ def send_help(message):
         "*Список команд, які використовує бот*:\n\n"
         "/add _назва, ціна, опис, кількість_ \- додати товар\n"
         "/delete _назва\\[, назва2, \\.\\.\\.\\]_ \- видалити товар\\[и\\]\n"
-        "/price _назва, ціна_ \- редагувати ціну товару\n"
-        "/desc _назва, опис_ \- редагувати опис товару\n"
-        "/stock _назва, кількість_ \- змінити кількість товару на складі\n"
-        "/report \- звіт по товарам на складі\n"
         "/print \- вивести список товарів",
         parse_mode="MarkdownV2")
 
@@ -35,8 +31,7 @@ def send_welcome(message):
         message.chat.id,
         f"{hello()}, {message.from_user.first_name}!\n"
         "Цей бот вміє працювати зі списком товарів. "
-        "Додавати, видаляти, редагувати товари, "
-        "змінювати їх кількість на складі, а також проводити базову аналітику. "
+        "Додавати та видаляти товари"
         "Наберіть /help для отримання допомоги по командам")
 
 
@@ -54,17 +49,14 @@ def add_goods(message):
                          f"Товар '{new_article[0]}' вже є у списку")
         return
     new_article[1] = set_price(new_article[1])
-    if new_article[1] <= 0:
-        bot.send_message(message.chat.id, "Хибна ціна товару")
-        return
     new_article[3] = set_stock(new_article[3])
-    if new_article[3] <= 0:
-        bot.send_message(message.chat.id, "Хибна кількість товару")
+    if new_article[1] < 0 or new_article[3] < 0:
+        bot.send_message(message.chat.id, "Хибна ціна або кількість товару")
         return
     goods.append(dict(zip(GOODS_KEYS, new_article)))
     save(goods)
     bot.send_message(message.chat.id,
-                     f"'{goods[-1][PRIMARY_KEY]}' додано до списку товарів")
+                     f"'{goods[-1]}' додано до списку товарів")
 
 
 @bot.message_handler(commands=["print"])
@@ -96,44 +88,6 @@ def delete_goods(message):
     save(goods)
 
 
-@bot.message_handler(commands=["price"])
-def change_price(message):
-    print(f"Обробка команди /price від {message.from_user.first_name}")
-    change_key("Ціна", message, set_price)
-
-
-@bot.message_handler(commands=["desc"])
-def change_desc(message):
-    print(f"Обробка команди /desc від {message.from_user.first_name}")
-    change_key("Опис", message)
-
-
-@bot.message_handler(commands=["stock"])
-def change_desc(message):
-    print(f"Обробка команди /stock від {message.from_user.first_name}")
-    change_key("Кількість", message, set_stock)
-
-
-@bot.message_handler(commands=["report"])
-def print_report(message):
-    global goods
-    print(f"Обробка команди /report від {message.from_user.first_name}")
-    total_goods = len(goods)
-    total_stock = sum([g["Кількість"] for g in goods])
-    total_value = sum([g["Ціна"] * g["Кількість"] for g in goods])
-    sorted_goods = sorted([(g["Назва"], g["Ціна"]) for g in goods],
-                          key=lambda item: item[1])
-    bot.send_message(
-        message.chat.id,
-        f"Всього найменувань товарів: {total_goods}\n"
-        f"Загальна кількість товарів на складі: {total_stock} шт.\n"
-        f"Ціна складу: {total_value} грн.\n"
-        f"Найдорожчий товар: '{sorted_goods[-1][0]}' "
-        f"з ціною {sorted_goods[-1][1]} грн.\n"
-        f"Найдешевший товар: '{sorted_goods[0][0]}' "
-        f"з ціною {sorted_goods[0][1]} грн.\n")
-
-
 @bot.message_handler(func=lambda message: message.text.lower() in HELLO_WORDS)
 def send_hello(message):
     name = message.from_user.first_name
@@ -160,23 +114,19 @@ def hello():
 
 
 def load():
-    try:
-        with open(GOODS_FILE_NAME, "r", encoding="utf8") as g:
-            loaded_goods = json.load(g)
-    except FileNotFoundError:
-        print(f"{GOODS_FILE_NAME} не знайдено.")
-        loaded_goods = []
+    global GOODS_FILE_NAME
+    with open(GOODS_FILE_NAME, "r", encoding="utf8") as g:
+        loaded_goods = json.load(g)
     print("Завантажено список товарів:\n"
           f"{json.dumps(loaded_goods, indent=2, ensure_ascii=False)}")
     return loaded_goods
 
 
 def save(json_obj):
-    try:
-        with open(GOODS_FILE_NAME, "w", encoding="utf8") as f:
-            json.dump(json_obj, f, indent=2, ensure_ascii=False)
-    except IOError as err:
-        print(f"Помилка запису {GOODS_FILE_NAME} {err}")
+    global GOODS_FILE_NAME
+    with open(GOODS_FILE_NAME, "w", encoding="utf8") as f:
+        json.dump(json_obj, f, indent=2, ensure_ascii=False)
+    print("Збережено список товарів")
 
 
 def parse_command_args(text):
@@ -188,11 +138,11 @@ def parse_command_args(text):
     return args
 
 
-def skip_command_and_split(text: str, split_symbol: str) -> list:
+def skip_command_and_split(text, split_symbol):
     return text.strip()[text.find(" ") + 1:].split(split_symbol)
 
 
-def delete_spaces(text: str) -> str:
+def delete_spaces(text):
     return " ".join(text.split())
 
 
@@ -216,32 +166,6 @@ def set_stock(text_stock):
         return int(text_stock)
     except ValueError:
         return -1
-
-
-def change_key(key, message, func=None):
-    global goods
-    args = parse_command_args(message.text)
-    if len(args) != 2:
-        bot.send_message(message.chat.id, "Повинно бути два аргументи")
-        return
-    index = find_goods(args[0])
-    if index == -1:
-        bot.send_message(
-            message.chat.id,
-            f"'{args[0]}' не знайдено у списку товарів")
-        return
-    if func:
-        value = func(args[1])
-        if value <= 0:
-            bot.send_message(message.chat.id,
-                             f"Другий аргумент потрібен бути позитивним числом")
-            return
-        goods[index][key] = value
-    else:
-        goods[index][key] = args[1]
-    bot.send_message(message.chat.id,
-                     f"{key} товару '{args[0]}' змінено на '{args[1]}'")
-    save(goods)
 
 
 if __name__ == "__main__":
